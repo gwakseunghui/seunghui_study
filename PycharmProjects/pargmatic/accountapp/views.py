4280bf3b704175a9378f5c3da802088328eb8182
gwakseunghui <seunghui.gwak@vntgcorp.com>
2021-08-01 23:32:09 +0900

    CHG: 210801_seunghui_TIL - django course 25 commit

--- PycharmProjects/pargmatic/accountapp/views.py
+++ PycharmProjects/pargmatic/accountapp/views.py
@@ -6,8 +6,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
@@ -40,7 +41,11 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
-
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
 
 
 
