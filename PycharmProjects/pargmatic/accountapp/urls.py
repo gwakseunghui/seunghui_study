fddff95d046a4b74307f3dbc32b2beb21be2d7d9
gwakseunghui <seunghui.gwak@vntgcorp.com>
2021-08-01 15:51:16 +0900

    CHG: 210801_seunghui_TIL - django course 16 commit

--- PycharmProjects/pargmatic/accountapp/urls.py
+++ PycharmProjects/pargmatic/accountapp/urls.py
@@ -5,5 +5,5 @@ from accountapp.views import hello_world
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world')
 ]
